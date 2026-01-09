3c600437c4d8827c26ba2ee30ec5f05634fcfc60
honeylatte0619 <honeylatte0619@gmail.com>
2026-01-10 08:51:01 +0900

    fix: Move MudThemeProvider to MainLayout and force cache bust

--- IsekaiStatusMaker/obj/Release/net8.0/IsekaiStatusMaker.AssemblyInfo.cs
+++ IsekaiStatusMaker/obj/Release/net8.0/IsekaiStatusMaker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IsekaiStatusMaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5dbc420d45790d06b60e84885254cf194ee3271")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f6bd49f4056521bdc01a693accef7c4b3ad993a")]
 [assembly: System.Reflection.AssemblyProductAttribute("IsekaiStatusMaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IsekaiStatusMaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
